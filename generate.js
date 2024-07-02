1437ba7ed6c7951e25d845dbccffd286d56e640b
Reginger92 <simone.congiu16@gmail.com>
2024-07-03 00:03:11 +0200

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,5 +1,5 @@
 const OpenAI = require('openai');
-require('dotenv').config(); // Load environment variables from .env file
+/*require('dotenv').config(); // Load environment variables from .env file*/
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY, // Access API key from environment variable
